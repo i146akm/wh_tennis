fa05aeb03b114d8917e02bd02d3df6d90cbbe374
IkolMe <turarbekovaikol@gmail.com>
2025-01-24 16:09:02 +0600

    Login.html added

--- tennis/data_parser.py
+++ tennis/data_parser.py
@@ -5,11 +5,9 @@ def live_tennis_data():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto('https://sports.williamhill.com/betting/en-gb/tennis')
-        page.wait_for_selector('#in-play-now', timeout=10000)
+        page.goto('https://sports.williamhill.com/betting/en-gb/in-play/tennis')
 
-        in_play_now = page.query_selector('#in-play-now')
-        live_events = in_play_now.query_selector_all('.event') if in_play_now else []
+        live_events = page.query_selector_all('.event')
 
         data = []
         for event in live_events:
@@ -44,11 +42,11 @@ def highlights_data():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto('https://sports.williamhill.com/betting/en-gb/tennis')
+        page.goto('https://sports.williamhill.com/betting/en-gb/tennis/matches')
         page.wait_for_selector('#match-highlights', timeout=10000)
 
         match_highlights_block = page.query_selector('#match-highlights')
-        match_highlights = match_highlights_block.query_selector_all('.event') if match_highlights_block else []
+        match_highlights = page.query_selector_all('.event')
 
         data = []
         for event in match_highlights:
